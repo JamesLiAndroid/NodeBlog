63c34f41042fc176c1416afacb9939e723d0c980
jyj-lsy <jyj-lsy@debian.debian>
2016-12-19 16:42:14 +0800

    get方法获取参数的方式区分

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ app.get('/', (req, res) => {
   res.send('hello, express')
 })
 
+app.get('/name/:name', (req,res) => {
+  res.send('hello,'+req.params.name)
+})
+
+app.get('/tag', (req,res) => {
+  res.send('hello tag:'+req.query.tag)
+})
+
 app.listen(3000)
