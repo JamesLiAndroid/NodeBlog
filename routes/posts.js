b42915d81bcbe5664e48b0ab68c108277b5dbaff
jyj-lsy <jyj-lsy@debian.debian>
2016-12-22 15:44:33 +0800

    完成发表文章的操作

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 var checkLogin = require('../middlewares/check').checkLogin
-
+var PostModel = require('../models/posts')
 // GET /posts 所有用户或者特定用户的文章页
 // //   eg: GET /posts?author=xxx
 router.get('/', (req,res,next) => {
@@ -12,12 +12,44 @@ router.get('/', (req,res,next) => {
 
 // POST /posts 发表一篇文章
 router.post('/', checkLogin, (req,res,next) => {
-  res.send(req.flash())
+  var author = req.session.user._id
+  var title = req.fields.title
+  var content = req.fields.content
+
+  // 校验参数
+  try {
+    if(!title.length) {
+      throw new Error('请填写标题')
+    }
+    if(!content.length) {
+      throw new Error('请填写内容')
+    }
+  } catch(e) {
+    req.flash('error', e.message)
+    return res.redirect('back')
+  }
+
+  var post = {
+    author: author,
+    title: title,
+    content: content,
+    pv: 0
+  }
+
+  PostModel.create(post)
+    .then((result) => {
+      // 此post是插入mongodb之后的值
+      post = result.ops[0]
+      req.flash('success', '发表成功')
+      // 发表成功后跳转到该文章页面
+      res.redirect('/posts/${post._id}')
+    })
+    .catch(next)
 })
 
 // GET /posts/create 发表文章页
 router.get('/create', checkLogin, (req,res,next) => {
-  res.send(req.flash())
+  res.render('create')
 })
 
 // GET /posts/:postId 单独一篇的文章页
--- routes/signout.js
+++ routes/signout.js
@@ -5,7 +5,12 @@ var checkLogin = require('../middlewares/check').checkLogin
 
 // GET /signout 登出
 router.get('/', checkLogin, (req,res,next) => {
-  res.send(req.flash())
+//   res.send(req.flash())
+  // 清空session中的用户信息
+  req.session.user = null
+  req.flash('success', '登出成功')
+  // 登出成功后跳转主页
+  res.direct('/posts')
 })
 
 module.exports = router
